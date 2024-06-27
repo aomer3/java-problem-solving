ae61e68ba7c88a96044e18ed92dc954b8a327168
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-06-27 11:32:50 -0400

    Add find duplicates function to RemoveDuplicates

--- JavaProblemSolving/src/udemy/Karimov/DSA/arrays/RemoveDuplicates.java
+++ JavaProblemSolving/src/udemy/Karimov/DSA/arrays/RemoveDuplicates.java
@@ -14,15 +14,20 @@ import java.util.Arrays;
 public class RemoveDuplicates {
 
 	public static void main(String[] args) {
-		displayRemoveDuplicates();
+		int[] numbers = {1, 1, 2, 2,3, 4, 5};
+		
+		System.out.print("Array: ");
+		displayArray(numbers);
+		displayDuplicates(numbers);
+		displayRemoveDuplicates(numbers);
 
 	}
 	
-	public static void displayRemoveDuplicates() {
-		int[] numbers = {1, 1, 2, 2 , 3, 4, 5};
+	public static void displayRemoveDuplicates(int[] numbers) {
 		int[] result = removeDuplicates(numbers);
 		
-		System.out.println(Arrays.toString(result));
+		System.out.print("Without duplicates: ");
+		displayArray(result);
 	}
 	
 	public static int[] removeDuplicates(int[] array) {
@@ -44,25 +49,54 @@ public class RemoveDuplicates {
 		return result;
 	}
 	
-	public static int[] removeZeros(int[] newArray) {
+	public static int[] removeZeros(int[] array) {
 		int count=0;
-		for(int num: newArray) {
+		for(int num: array) {
 			if(num == 0) {
 				count++;
 			}
 		}
 		
-		int result[] = new int[newArray.length - count];
+		int result[] = new int[array.length - count];
 		int index=0;
 		
-		for(int i=0; i < newArray.length; i++) {
-			if(newArray[i] != 0) {
-				result[index] = newArray[i];
+		for(int i=0; i < array.length; i++) {
+			if(array[i] != 0) {
+				result[index] = array[i];
 				index++;
 			}
 		}
 		
 		return result;
+	}
+	
+	public static void displayDuplicates(int[] numbers) {
+		int[] newArray = findDuplicates(numbers);
+		int[] result = removeZeros(newArray);
+		System.out.print("Duplicates: " );
+		displayArray(result);
+	}
+	
+	public static int[] findDuplicates(int[] array) {
+		int[] newArray = new int[array.length];
+		int j=0;
+		
+		for(int i=0; i < array.length-1; i++) {
+			if(array[i] == array[i+1]) {
+				newArray[j++] = array[i];
+			}
+		}
+		
+		
+		
+		return newArray;
+	}
+	
+	public static void displayArray(int[] array) {
+		for(int num: array) {
+			System.out.print(num + " ");
+		}
+		System.out.println(" ");
 	}
 
 }
