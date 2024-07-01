f8e2b43ddb77f85d3515f32046e86710fdd7e063
aomer3 <67030297+aomer3@users.noreply.github.com>
2024-06-30 21:05:29 -0400

    Find middle node of LinkedList using Floyd's Tortoise and Hare algorithm

--- JavaProblemSolving/src/udemy/BarrettDSA/linkedlists/ALinkedList.java
+++ JavaProblemSolving/src/udemy/BarrettDSA/linkedlists/ALinkedList.java
@@ -199,5 +199,55 @@ public class ALinkedList {
 		}
 		
 	}
+	
+	/*Implement a method called findMiddleNode that returns the middle node of the linked list.
+
+	If the list has an even number of nodes, the method should return the second middle node.
+	
+	Note: this LinkedList implementation does not have a length member variable.
+	
+	
+	Method signature:
+	
+	public Node findMiddleNode()
+	
+	Example:
+	
+	LinkedList myList = new LinkedList(1);
+	myList.append(2);
+	myList.append(3);
+	myList.append(4);
+	myList.append(5);
+	Node middleNode = myList.findMiddleNode();
+	System.out.println(middleNode.value); // Output: 3
+	 
+	myList.append(6);
+	middleNode = myList.findMiddleNode();
+	System.out.println(middleNode.value); // Output: 4
+	
+	
+	When the linked list has an odd number of nodes, like 1 -> 2 -> 3 -> 4 -> 5, the function will find the exact middle node. 
+	In this case, it will return the node containing the value 3.
+	
+	When the linked list has an even number of nodes, there will be two middle nodes. The findMiddleNode function will return the 
+	second of these two middle nodes.
+	
+	For example, if the linked list is 1 -> 2 -> 3 -> 4 -> 5 -> 6, the two middle nodes are 3 and 4. The function will return the 
+	node containing the value 4.
+	 * 
+	 */
+	public int findMiddleNode() {
+		 Node tortoise = head;
+		 Node hare = head;
+		 
+		 while(hare != null && hare.next != null) {
+			 tortoise = tortoise.next;
+			 hare = hare.next;
+			 hare = hare.next; 
+		 }
+		 
+		 return tortoise.value;
+	}
+	
 
 }
--- JavaProblemSolving/src/udemy/BarrettDSA/linkedlists/main.java
+++ JavaProblemSolving/src/udemy/BarrettDSA/linkedlists/main.java
@@ -4,12 +4,16 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		ALinkedList alist = new ALinkedList(4);
+		ALinkedList alist = new ALinkedList(1);
 		
-		alist.prepend(9);
-		alist.insert(1, 5);
+		alist.append(2);
+		alist.append(3);
+		alist.append(4);
+		alist.append(5);
+//		alist.append(6);
 		
 		alist.printAll();
+		System.out.println("Middle: " + alist.findMiddleNode());
 	}
 
 }
